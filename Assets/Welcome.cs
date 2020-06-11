f87df9e6beaa89c4376a7b6977e9d20919c9587b
Moses <paekonde@gmail.com>
2020-06-11 15:17:25 +0200

    Initial backup of the demo game

---
+++ Assets/Welcome.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Welcome : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Return))
+        {
+            StartGame();
+        }
+    }
+}
